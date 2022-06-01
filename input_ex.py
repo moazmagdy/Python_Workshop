5fcb7c895c07d2622c34600efce66f8e919b105b
moaz.magdy <44733972+moazmagdy@users.noreply.github.com>
2022-06-01 21:39:24 +0200

    modify

---
+++ input_ex.py
@@ -0,0 +1,2 @@
+rate = input("Please, enter your rate from 1 to 10: ")
+print("Your rating is {}. Thank you!".format(rate))
--- vital_python.py
+++ vital_python.py
@@ -9,6 +9,23 @@ Concepts
 # import keyword
 # print(keyword.kwlist)
 
-print("Here we use \\t \t to insert tab string!")
-print("Here we use \\n \n to add new line!")
-print("Here we use \\v \v to insert vertical tab!")
+# print("Here we use \\t \t to insert tab string!")
+# print("Here we use \\n \n to add new line!")
+# print("Here we use \\v \v to insert vertical tab!")
+multiline_string = '''
+During our vacation to San Francisco, we waited in a long line by
+Powell St. Station to take the cable car. Tap dancers performed on
+wooden boards. By the time our cable car arrived, we started looking
+online for a good place to eat. We're heading to North Beach
+'''
+# print(multiline_string)
+
+# print("Hi" + " MOHSEN! " * 3)
+
+first_name = "Moaz"
+last_name = "Magdy"
+
+# print("Hi! {0} {1}, How are you?".format(first_name,last_name))
+# print(first_name.lower())
+# print(first_name.count('o'))
+# print(first_name.find('z'))
