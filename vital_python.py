b8ab07f765ca24544f06342e6e387d8ec7200781
moaz.magdy <44733972+moazmagdy@users.noreply.github.com>
2022-06-01 21:16:29 +0200

    Modified

--- vital_python.py
+++ vital_python.py
@@ -6,5 +6,9 @@ Concepts
 # import math
 # print(math.pow(4,2))
 # print(math.ceil(9/5))
-import keyword
-print(keyword.kwlist)
+# import keyword
+# print(keyword.kwlist)
+
+print("Here we use \\t \t to insert tab string!")
+print("Here we use \\n \n to add new line!")
+print("Here we use \\v \v to insert vertical tab!")
