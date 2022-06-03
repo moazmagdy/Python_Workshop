3097e8d13f0d32293b9a4b064b633be109f7daaf
moaz.magdy <44733972+moazmagdy@users.noreply.github.com>
2022-06-03 14:58:40 +0200

    modify

--- vital_python.py
+++ vital_python.py
@@ -113,6 +113,17 @@ last_name = "Magdy"
 #     print("Department: ", employee_data[i][2])
 #     print("-------------------------")
 
-X = [[1,2,3],[4,5,6],[7,8,9]]
-Y = [[10,11,12],[13,14,15],[16,17,18]]
+#Addition of two matrices
 
+x = [[1,2,3],[4,5,6],[7,8,9]]
+y = [[10,11,12],[13,14,15],[16,17,18]]
+
+result= [[0,0,0],
+        [0,0,0],
+        [0,0,0]]
+
+for i in range(len(x)):
+    for j in range(len(x[i])):
+        result[i][j]= x[i][j] + y[i][j]
+
+print(result)
