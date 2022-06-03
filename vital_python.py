2f96c6c5b8c3a4eff5c2ef60fee67ace0fd86bf0
moaz.magdy <44733972+moazmagdy@users.noreply.github.com>
2022-06-03 14:59:40 +0200

    modify

--- vital_python.py
+++ vital_python.py
@@ -115,15 +115,15 @@ last_name = "Magdy"
 
 #Addition of two matrices
 
-x = [[1,2,3],[4,5,6],[7,8,9]]
-y = [[10,11,12],[13,14,15],[16,17,18]]
+# x = [[1,2,3],[4,5,6],[7,8,9]]
+# y = [[10,11,12],[13,14,15],[16,17,18]]
 
-result= [[0,0,0],
-        [0,0,0],
-        [0,0,0]]
+# result= [[0,0,0],
+#         [0,0,0],
+#         [0,0,0]]
 
-for i in range(len(x)):
-    for j in range(len(x[i])):
-        result[i][j]= x[i][j] + y[i][j]
+# for i in range(len(x)):
+#     for j in range(len(x[i])):
+#         result[i][j]= x[i][j] + y[i][j]
 
-print(result)
+# print(result)
