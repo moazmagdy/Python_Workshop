ccfaf95ebb9a73333ee8c1153ed6fdb8001f6a09
moaz.magdy <44733972+moazmagdy@users.noreply.github.com>
2022-06-04 11:07:38 +0200

    modify

--- python_structures.py
+++ python_structures.py
@@ -17,7 +17,6 @@
 #     print("Department: ", employee_data[i][2])
 #     print("-------------------------")
 
-#Addition of two matrices
 
 x = [[1,2,3],[4,5,6],[7,8,9]]
 y = [[10,11,12],[13,14,15],[16,17,18]]
@@ -26,8 +25,20 @@ result= [[0,0,0],
         [0,0,0],
         [0,0,0]]
 
-for i in range(len(x)):
-    for j in range(len(x[i])):
-        result[i][j]= x[i][j] + y[i][j]
+#Addition of two matrices
+# for i in range(len(x)):
+#     for j in range(len(x[i])):
+#         result[i][j]= x[i][j] + y[i][j]
+
+#Multiplication of two matrices
+
+for i in range(len(result)):
+     for j in range(len(x[0])):
+         for v in range(len(x[0])):
+             result[i][j] += x[i][j+v] * y[i+v][j]
+             print(result)
+             print(i,j,v)
+         j += 1
+     i += 1
 
 print(result)
