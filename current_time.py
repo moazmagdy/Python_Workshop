0cc21d17017f705762f7049b09b88be8187d51c2
moaz.magdy <44733972+moazmagdy@users.noreply.github.com>
2022-06-10 21:29:02 +0200

    modify

--- current_time.py
+++ current_time.py
@@ -3,5 +3,11 @@ This module outputs the current system time.
 """
 import datetime
 time = datetime.datetime.now().time()
+"""
+if __name__ == '__main__
+It is used when you want to be able to execute
+the script by itself, but also be able to import objects from the script as though it were a
+regular module.
+"""
 if __name__ == '__main__':
     print(time)
