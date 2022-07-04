c6a6687854ce97291a6b8b935e8bec3f193d7924
moaz.magdy <44733972+moazmagdy@users.noreply.github.com>
2022-07-04 21:13:35 +0200

    new

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+"""
+This function returns nth fibonacci number using the
+golden ratio (1.618034)
+"""
+
+def fibonacci(n):
+    return print(int((1.618034**n - (1- 1.618034)**n)/5**0.5))
+
+fibonacci(10)
