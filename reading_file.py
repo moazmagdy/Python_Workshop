f437d87dc2600546f8a947db6c177c87d7556cc4
moaz.magdy <44733972+moazmagdy@users.noreply.github.com>
2022-07-11 14:07:15 +0200

    modify

---
+++ reading_file.py
@@ -0,0 +1,14 @@
+"""
+This script open, read, and close files.
+"""
+# f = open('pg37431.txt')
+# text = f.read()
+# print(text)
+
+# f.close()
+
+# with open('pg37431.txt') as f:
+#     print(f.read())
+
+with open('pg37431.txt') as f:
+    print(f.readline())
