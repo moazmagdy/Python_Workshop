b8b703a4c61aac85ece3920ea9495a0d8ee8ffef
moaz.magdy <44733972+moazmagdy@users.noreply.github.com>
2022-07-11 13:12:12 +0200

    mod

--- filter_fn.py
+++ filter_fn.py
@@ -4,9 +4,10 @@ This script includes implementations of filter function.
 #We use filter function to return only odd numbers
 #from a list of numbers
 nums = [1,2,3,4,5,6,7,8,9]
-list(filter(lambda x: x % 2 != 0, nums))
+odd_nums = list(filter(lambda x: x % 2 != 0, nums))
+sorted(odd_nums, reverse= True)
 
 #We use filter function to sum all multiples of 3 in 
 #numbers under 1000.
 nums = list(range(1000))
-sum(list(filter(lambda x: x % 3 == 0, nums)))
+sum_multiplies = sum(list(filter(lambda x: x % 3 == 0, nums)))
