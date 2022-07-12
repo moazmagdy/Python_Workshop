5f1a611d2b7645b970609a05d9601ac1f3c8afa2
moaz.magdy <44733972+moazmagdy@users.noreply.github.com>
2022-07-12 10:20:15 +0200

    modify

--- circle_class.py
+++ circle_class.py
@@ -1,16 +1,24 @@
+import math
 class circle():
     """
     A class for circles with radius and color as attributes.
     """
+
     is_shape = True
 
     def __init__(self, radius, color):
         self.radius = radius
         self.color = color
+    
+    def area(self):
+        return round(math.pi * self.radius**2,2)
 
 first_circle = circle(2,"blue")
 second_circle = circle(5, "red")
 
+first_circle.radius = 12
+
 print("\n",
     'First circle attributes are:', first_circle.__dict__, "\n",
-    'Second circle attributes are:', second_circle.__dict__,"\n")
+    'Second circle attributes are:', second_circle.__dict__,"\n",
+    "First Circle area is:", first_circle.area(), "mm\u00b2")
