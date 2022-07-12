c74f01504c06d1d5679f68c9b672d4d5eb57db7a
moaz.magdy <44733972+moazmagdy@users.noreply.github.com>
2022-07-12 10:57:15 +0200

    modify

--- pet_class.py
+++ pet_class.py
@@ -1,7 +1,6 @@
 """
 This script creates a class of my pets with common attributes.
 """
-
 class pet():
     """
     A class to capture useful information regarding my pets, just incase
@@ -10,18 +9,19 @@ class pet():
     is_human = False
     owner = "Moaz Magdy"
 
-    def __init__(self, height= 5, gender='unknown'):
+    def __init__(self, height, gender='unknown'):
         self.height = height    #In centimeters
         self.gender = gender    #Only male or female
 
-    def is_tall(self):
-        return self.height >= 30
+    def is_tall(self, tall_if_at_least):
+        return self.height >= tall_if_at_least
 
 aww = pet(height= 15, gender= "male")
+aww.is_tall(30)
 
 print(aww.is_human,"\n",
         aww.owner,"\n",
         aww.__doc__, "\n",
         aww.height, "\n",
         aww.gender, "\n",
-        aww.is_tall())
+        aww.is_tall(30))
