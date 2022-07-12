529f0f5fb065b328c8c7402d0eb72d02a4798b04
moaz.magdy <44733972+moazmagdy@users.noreply.github.com>
2022-07-12 10:24:17 +0200

    modify

--- pet_class.py
+++ pet_class.py
@@ -7,17 +7,21 @@ class pet():
     A class to capture useful information regarding my pets, just incase
     I lose track of them.
     """
-    def __init__(self, height= 5, gender='unknown'):
-        self.height = height
-        self.gender = gender
-
     is_human = False
     owner = "Moaz Magdy"
 
-aww = pet(height= 15)
+    def __init__(self, height= 5, gender='unknown'):
+        self.height = height    #In centimeters
+        self.gender = gender    #Only male or female
+
+    def is_tall(self):
+        return self.height >= 30
+
+aww = pet(height= 15, gender= "male")
 
 print(aww.is_human,"\n",
         aww.owner,"\n",
         aww.__doc__, "\n",
         aww.height, "\n",
-        aww.gender)
+        aww.gender, "\n",
+        aww.is_tall())
