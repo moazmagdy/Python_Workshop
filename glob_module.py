ba09d672aee7b0ec6454e92e8bc20d9ebef8f7ce
moaz.magdy <44733972+moazmagdy@users.noreply.github.com>
2022-07-20 19:13:45 +0200

    modify

---
+++ glob_module.py
@@ -0,0 +1,4 @@
+import glob
+import os
+os.chdir("D:\Downloads")
+print(glob.glob('*python*'))
--- std_lib.py
+++
@@ -1,4 +0,0 @@
-import os
-os.chdir('D:\Github\leetcode_problems')
-# print(os.getcwd())  #To print current working directory
-# print(dir(os))    #To print all functions
